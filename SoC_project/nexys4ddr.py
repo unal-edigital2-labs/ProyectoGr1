961a70bdf57a83f6ef9026a1e539b31a9cdfaa7b
juochoac <juochoac@unal.edu.co>
2022-11-24 02:45:46 -0500

    a

--- SoC_project/nexys4ddr.py
+++ SoC_project/nexys4ddr.py
@@ -1,4 +1,4 @@
-8from litex.build.generic_platform import *
+from litex.build.generic_platform import *
 from litex.build.xilinx import XilinxPlatform, VivadoProgrammer
 
 # IOs ----------------------------------------------------------------------------------------------
